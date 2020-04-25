8faef4854c41307cc33f62fb6e3f90465e09842b
wh.zhao <wh.zhao@trip.com>
2020-04-25 21:00:18 +0800

    设置元素特征attrs

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
-import React from "../lib/react"
-import ReactDOM from "../lib/react-dom"
+import React from "../lib/react";
+import ReactDOM from "../lib/react-dom";
 
-const vDom = <div><h1>一条龙</h1>React</div>
-
-ReactDOM.render(vDom, document.getElementById("root"))
+const vDom = (
+  <div>
+    <h1 style={{ color: "red" }}>一条龙</h1>
+    <span className="big">React</span>
+  </div>
+);
+console.log(vDom)
+ReactDOM.render(vDom, document.getElementById("root"));
