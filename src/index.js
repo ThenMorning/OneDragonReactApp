aab8a9cf67cfb820b43b17e6176a3058f7b0efd5
wh.zhao <wh.zhao@trip.com>
2020-04-25 20:10:11 +0800

    实现ReactDOM.render方法

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import React from "../lib/react"
+import ReactDOM from "../lib/react-dom"
 
-const vDom = <div>test</div>
-console.log(vDom)
+const vDom = <div><h1>一条龙</h1>React</div>
+
+ReactDOM.render(vDom, document.getElementById("root"))
