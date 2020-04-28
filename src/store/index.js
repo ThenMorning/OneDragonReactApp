c49e5577e62209ed2b163595188c77716eac77f6
wh.zhao <wh.zhao@trip.com>
2020-04-28 21:03:59 +0800

    (二十二)Redux 插件封装dispatch

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,14 @@
 import { createStore } from "../../lib/redux";
 import reducer from "./reducer";
+import thunk from "../../lib/redux-thunk"
+let p2 = (dispatch)=>{
+    return (action)=>{
+        console.log("p2")
+        dispatch(action)
+    }
+}
+export default createStore({a:1},reducer,[
+    thunk,p2
+]);
 
-export default createStore({a:1},reducer);
+
