e61712b996bc3166f5a336d4a8aa8cedcc4a1101
wh.zhao <wh.zhao@trip.com>
2020-04-28 11:08:38 +0800

    (十七)React-redux实现connect联动更新

--- src/app.js
+++ src/app.js
@@ -5,16 +5,27 @@ class App extends React.Component {
   constructor(props) {
     super(props);
   }
-  
+
   render() {
     return (
       <div>
-        这是store{this.props.a}
+        <ConnectSon1 />
+        <ConnectSon2 />
+      </div>
+    );
+  }
+}
+
+class Son1 extends React.Component {
+  render() {
+    return (
+      <div>
+        Son1 {this.props.a}
         <button
           onClick={() => {
             this.props.setStore({
               store: {
-                a: this.props.a+1,
+                a: this.props.a + 1,
               },
             });
           }}
@@ -25,12 +36,52 @@ class App extends React.Component {
     );
   }
 }
-export default connnect(
+
+class Son2 extends React.Component {
+  render() {
+    return (
+      <div>
+        Son2 {this.props.a}
+        <button
+          onClick={() => {
+            this.props.setStore({
+              store: {
+                a: this.props.a + 2,
+              },
+            });
+          }}
+        >
+          click
+        </button>
+      </div>
+    );
+  }
+}
+
+const ConnectSon1 = connnect(
   (store) => {
     return store;
   },
   (util) => {
-    console.log(util)
     return util;
   }
-)(App);
+)(Son1);
+const ConnectSon2 = connnect(
+  (store) => {
+    return store;
+  },
+  (util) => {
+    return util;
+  }
+)(Son2);
+export default App;
+
+// export default connnect(
+//   (store) => {
+//     return store;
+//   },
+//   (util) => {
+//     console.log(util)
+//     return util;
+//   }
+// )(App);
